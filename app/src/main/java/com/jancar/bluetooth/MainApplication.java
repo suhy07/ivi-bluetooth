0f6f07bc04eb66673f0f8a3538d19ba0b14d6151
liyongde <li.yongde@jancar.com.cn>
2023-12-12 15:58:11 +0800

    Auto连接状态，在auto界面打电话，不弹蓝牙电话弹窗

--- app/src/main/java/com/jancar/bluetooth/MainApplication.java
+++ app/src/main/java/com/jancar/bluetooth/MainApplication.java
@@ -1,11 +1,13 @@
 package com.jancar.bluetooth;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -18,10 +20,13 @@ import com.jancar.sdk.BaseManager;
 import com.jancar.sdk.bluetooth.BluetoothManager;
 import com.jancar.sdk.bluetooth.IVIBluetooth;
 import com.jancar.sdk.system.IVISystem;
+import com.jancar.sdk.utils.ActivityUtils;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.List;
 
 
 /**
@@ -114,7 +119,12 @@ public class MainApplication extends Application {
                 if(isBackCar){
                     mCallWindowUtil.showSmallCallWindow();
                 }else{
-                    mCallWindowUtil.showCallWindow();
+                    if(isAutoConnected() && isInAutoScreen()){
+
+                    }else{
+                        mCallWindowUtil.showCallWindow();
+                    }
+
                 }
             }
 
@@ -151,9 +161,13 @@ public class MainApplication extends Application {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
+            Log.i("MainApplication", "onReceive action:"+action);
             if(IVISystem.ACTION_BACKCAR_FINISH.equals(action)){
                 isBackCar = false;
                 mCallWindowUtil.setBackCar(false);
+                if(isAutoConnected() && isInAutoScreen()){
+                    mCallWindowUtil.hideSmallCallWindow();
+                }
             }else if(IVISystem.ACTION_BACKCAR_STARTED.equals(action)){
                 isBackCar = true;
                 mCallWindowUtil.setBackCar(true);
@@ -170,6 +184,24 @@ public class MainApplication extends Application {
         Intent intent = new Intent();
         intent.setClass(this, CallActivity.class);
         startActivity(intent);
+    }
+
+
+    private boolean isAutoConnected(){
+        String phoneMode = Settings.Global.getString(getContentResolver(),IVISystem.KEY_PHONE_MODE);
+        Log.i("MainApplication", "isAutoConnected phoneMode:"+phoneMode);
+        if(phoneMode == null || phoneMode.equals("")){
+            return false;
+        }
+        return phoneMode.contains("auto");
+    }
+
+    private boolean isInAutoScreen(){
+        ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> appTask = am.getRunningTasks(1);
+        String topActivityName = ((ActivityManager.RunningTaskInfo)appTask.get(0)).topActivity.getClassName();
+        Log.i("MainApplication", "topActivityName:"+topActivityName);
+        return topActivityName.equals("com.google.android.projection.sink.ui.AndroidAutoActivity")||topActivityName.equals(IVISystem.ACTIVITY_ANDROID_AUTO);
     }
 
 
