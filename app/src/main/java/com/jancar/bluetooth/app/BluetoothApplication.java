79e6bd10e338ced61eddab67e899a15265d1e1bc
suhy <su.haiyuan@jancar.com.cn>
2024-04-05 10:18:28 +0800

    [UPDATE]去除main中的leakCanary

--- app/src/main/java/com/jancar/bluetooth/app/BluetoothApplication.java
+++ app/src/main/java/com/jancar/bluetooth/app/BluetoothApplication.java
@@ -25,7 +25,6 @@ import com.jancar.sdk.bluetooth.IVIBluetooth;
 import com.jancar.sdk.system.IVISystem;
 import com.jancar.sdk.system.SystemManager;
 import com.jancar.services.system.ISystemCallback;
-import com.squareup.leakcanary.LeakCanary;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -107,21 +106,6 @@ public class BluetoothApplication extends BaseApplication {
         mSystemManager = new SystemManager(mInstance, ConnectListen_System);
         //是否开启打印日志
         KLog.init(BuildConfig.DEBUG);
-    }
-
-    private void initCrash() {
-        CaocConfig.Builder.create()
-                .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT) //背景模式,开启沉浸式
-                .enabled(true) //是否启动全局异常捕获
-                .showErrorDetails(true) //是否显示错误详细信息
-                .showRestartButton(true) //是否显示重启按钮
-                .trackActivities(true) //是否跟踪Activity
-                .minTimeBetweenCrashesMs(2000) //崩溃的间隔时间(毫秒)
-                .errorDrawable(R.mipmap.ic_launcher) //错误图标
-                .restartActivity(MainActivity.class) //重新启动后的activity
-//                .errorActivity(YourCustomErrorActivity.class) //崩溃后的错误activity
-//                .eventListener(new YourCustomEventListener()) //崩溃后的错误监听
-                .apply();
     }
 
     BaseManager.ConnectListener ConnectListen_System = new BaseManager.ConnectListener() {
